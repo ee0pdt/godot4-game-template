06fb30d48bdef5bb0117a0f4b504198a1e303016
Pete Thorne <pete.thorne@learningpool.com>
2022-09-30 21:04:03 +0100

    Added splash screen

--- splash_screen/splash_screen.gd
+++ splash_screen/splash_screen.gd
@@ -1,10 +1,9 @@
 extends Node2D
 
 
-@export var main_menu: PackedScene
 @export var splash_time: float = 1.5
 
 
 func _ready():
 	await get_tree().create_timer(splash_time).timeout
-	get_tree().change_scene_to_packed(main_menu)
+	AppEvents.main_menu.emit()
