a3155656e592edc42ee8bc204ea99bc95d884a25
Pete Thorne <pete.thorne@learningpool.com>
2022-10-01 14:42:57 +0100

    Adding FPS elements

--- levels/level.gd
+++ levels/level.gd
@@ -1,5 +1,18 @@
-extends Node2D
+extends Node3D
+
+
+var countdown_secs: float = 10
 
 
 func _on_next_level_pressed():
 	GameEvents.next_level.emit()
+
+
+func _process(delta):
+	countdown_secs -= delta
+	_update_hud()
+
+
+func _update_hud():
+	var seconds_text: String = str(countdown_secs)
+	%Countdown.text = seconds_text
