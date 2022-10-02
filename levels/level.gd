e140f9c5439da2579a6345e023ee7e3cf37c1660
Pete Thorne <pete.thorne@learningpool.com>
2022-10-02 22:02:35 +0100

    Adding more button craziness

--- levels/level.gd
+++ levels/level.gd
@@ -10,6 +10,7 @@ enum States {
 var countdown_secs: float = 10
 var current_state = States.BEFORE
 var total_time: float = 0
+var button_hit: bool = false
 
 
 func _ready():
@@ -22,9 +23,10 @@ func _handle_button_pressed():
 		States.BEFORE:
 			$SoundMusic.play()
 			current_state = States.PLAYING
+			%Button/AnimationPlayer.play("ButtonRise")
 			$"3DWorld/AnimationPlayer".play("Moves")
 		States.PLAYING:
-			countdown_secs = 10
+			button_hit = true
 		States.OVER:
 			pass
 
@@ -38,12 +40,16 @@ func _process(delta):
 		States.PLAYING:
 			countdown_secs -= delta
 			if countdown_secs <= 0:
-				countdown_secs = 0
-				current_state = States.OVER
-				GameEvents.next_level.emit()
-			else:
-				total_time += delta
+				if button_hit:
+					countdown_secs = 10
+					%Button/AnimationPlayer.play("ButtonRise")
+					button_hit = false
+				else:
+					countdown_secs = 0
+					current_state = States.OVER
+					GameEvents.next_level.emit()
 			
+			total_time += delta
 			_update_hud()
 		_:
 			pass
