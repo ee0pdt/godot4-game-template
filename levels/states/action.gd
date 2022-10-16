5228f3e2bfb89af0edbb9bd74e4541393c5032bc
Pete Thorne <pete.thorne@learningpool.com>
2022-10-16 12:06:57 +0100

    Added dialog commands

--- levels/states/action.gd
+++ levels/states/action.gd
@@ -17,26 +17,27 @@ func _check_for_right_action():
 	match owner.current_action:
 		(owner as Level).Actions.RAISE:
 			if Input.is_action_just_released("raise"):
-				state_machine.transition_to(owner.States.keys()[owner.States.INSTRUCTING])
+				state_machine.transition_to(owner.States.keys()[owner.States.IDLE])
 			else:
 				_check_for_wrong_action()
 		(owner as Level).Actions.WAVE:
 			if Input.is_action_just_released("wave"):
-				state_machine.transition_to(owner.States.keys()[owner.States.INSTRUCTING])
+				state_machine.transition_to(owner.States.keys()[owner.States.IDLE])
 			else:
 				_check_for_wrong_action()
 		(owner as Level).Actions.DIP:
 			if Input.is_action_just_released("dip"):
-				state_machine.transition_to(owner.States.keys()[owner.States.INSTRUCTING])
+				state_machine.transition_to(owner.States.keys()[owner.States.IDLE])
 			else:
 				_check_for_wrong_action()
 		(owner as Level).Actions.BLESS:
 			if Input.is_action_just_released("bless"):
-				state_machine.transition_to(owner.States.keys()[owner.States.INSTRUCTING])
+				state_machine.transition_to(owner.States.keys()[owner.States.IDLE])
 			else:
 				_check_for_wrong_action()
 
 
+# If we are here we can assume that the player did not press the appropriate button
 func _check_for_wrong_action():
 	if Input.is_action_just_released("raise") or \
 		Input.is_action_just_released("wave") or \
--- levels/states/instructing.gd
+++ levels/states/instructing.gd
@@ -3,6 +3,16 @@ extends State
 
 func enter(_msg := {}) -> void:
 	owner.current_action = owner.Actions.values()[randi_range(0, owner.Actions.size()-1)]
+	
+	match owner.current_action:
+		(owner as Level).Actions.RAISE:
+			%DialogRaiseIt.play()
+		(owner as Level).Actions.WAVE:
+			%DialogWaveIt.play()
+		(owner as Level).Actions.DIP:
+			%DialogDipIt.play()
+		(owner as Level).Actions.BLESS:
+			%DialogBlessIt.play()
 
 
 func update(delta: float) -> void:
