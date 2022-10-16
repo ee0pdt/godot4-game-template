621637ba69e6a504fb939a703e5b7ed3935c040f
Pete Thorne <pete.thorne@learningpool.com>
2022-10-16 11:28:17 +0100

    Added unhappy path

--- levels/states/action.gd
+++ levels/states/action.gd
@@ -10,20 +10,36 @@ func update(delta: float) -> void:
 	if owner.countdown <= 0:
 		state_machine.transition_to(owner.States.keys()[owner.States.OVER])
 	else:
-		_check_actions()
+		_check_for_right_action()
 
 
-func _check_actions():
+func _check_for_right_action():
 	match owner.current_action:
 		(owner as Level).Actions.RAISE:
-			if Input.is_action_just_pressed("raise"):
+			if Input.is_action_just_released("raise"):
 				state_machine.transition_to(owner.States.keys()[owner.States.INSTRUCTING])
+			else:
+				_check_for_wrong_action()
 		(owner as Level).Actions.WAVE:
-			if Input.is_action_just_pressed("wave"):
+			if Input.is_action_just_released("wave"):
 				state_machine.transition_to(owner.States.keys()[owner.States.INSTRUCTING])
+			else:
+				_check_for_wrong_action()
 		(owner as Level).Actions.DIP:
-			if Input.is_action_just_pressed("dip"):
+			if Input.is_action_just_released("dip"):
 				state_machine.transition_to(owner.States.keys()[owner.States.INSTRUCTING])
+			else:
+				_check_for_wrong_action()
 		(owner as Level).Actions.BLESS:
-			if Input.is_action_just_pressed("bless"):
+			if Input.is_action_just_released("bless"):
 				state_machine.transition_to(owner.States.keys()[owner.States.INSTRUCTING])
+			else:
+				_check_for_wrong_action()
+
+
+func _check_for_wrong_action():
+	if Input.is_action_just_released("raise") or \
+		Input.is_action_just_released("wave") or \
+		Input.is_action_just_released("dip") or \
+		Input.is_action_just_released("bless"):
+			state_machine.transition_to(owner.States.keys()[owner.States.OVER])
