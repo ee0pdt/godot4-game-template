32e6d3f36a083e8cb6a6cc926d1247ed05532bb4
Pete Thorne <pete.thorne@learningpool.com>
2022-10-18 20:20:42 +0100

    Added correct game start behaviour

--- levels/states/before.gd
+++ levels/states/before.gd
@@ -14,3 +14,14 @@ func enter(_msg := {}) -> void:
 func update(delta: float) -> void:
 	if (owner as Level).has_hammer:
 		state_machine.transition_to((owner as Level).States.keys()[(owner as Level).States.IDLE])
+
+
+func handle_input(_event):
+	pass
+
+
+func exit():
+	%MusicAtmosphere.stop()
+	%DialogAnotherNewRecruit.stop()
+	%DialogDoNotBeNervous.stop()
+	%DialogWhenYouAreReady.stop()
