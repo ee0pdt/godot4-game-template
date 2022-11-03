110480a44e5001215d9008317693e4d09769b070
Pete Thorne <pete.thorne@learningpool.com>
2022-11-03 08:04:27 +0000

    Hooked up Done button in settings menu

--- settings/settings.gd
+++ settings/settings.gd
@@ -4,13 +4,6 @@ extends Node2D
 @export var game: PackedScene
 
 
-func _on_start_game_pressed():
-	AppEvents.start_game.emit()
-
-
-func _on_quit_pressed():
-	AppEvents.quit.emit()
-
-
-func _on_settings_button_pressed():
-	AppEvents.settings.emit()
+func _on_done_button_pressed() -> void:
+	AppEvents.main_menu.emit()
+	get_tree().paused = false
