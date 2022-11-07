4ba899b601e1e7eb6fb95420fc0d2215cbb637f6
Pete Thorne <pete.thorne@learningpool.com>
2022-11-07 21:11:24 +0000

    Simplified end game loading

--- game/game.gd
+++ game/game.gd
@@ -5,8 +5,6 @@ class_name Game
 @export var levels: Array[PackedScene]
 @export_file("*.tscn") var game_complete
 
-var game_complete_scene: PackedScene
-
 
 var current_level_scene: Node
 var current_level = 0
@@ -16,7 +14,6 @@ const PLAYER_NAME := "Player"
 
 
 func _ready() -> void:
-	game_complete_scene = load(game_complete)
 	_register_events()
 	current_level_scene = levels[0].instantiate()
 	add_child(current_level_scene)
@@ -33,7 +30,7 @@ func _handle_level_changed() -> void:
 	var next_scene: PackedScene
 
 	if current_level >= levels.size():
-		next_scene = game_complete_scene
+		next_scene = load(game_complete)
 	else:
 		next_scene = levels[current_level]
 	
