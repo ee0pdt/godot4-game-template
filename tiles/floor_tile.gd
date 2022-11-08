a0e423c805b2e51dd74bb77b7c4e4b64f7b37494
Pete Thorne <pete.thorne@learningpool.com>
2022-11-08 15:51:58 +0000

    Basic tiles in place and camera angle fixed

--- tiles/floor_tile.gd
+++ tiles/floor_tile.gd
@@ -5,8 +5,11 @@ extends MapTileItem
 @export_range(1, 3) var tile_size: int = 1:
 	set(tile_size_in):
 		tile_size = tile_size_in
-		CELL_SIZE = tile_size_in
-
-
-func _process(delta: float) -> void:
-	super._process(delta)
+		var radius = float(tile_size_in) / 2
+		
+		var mesh_node: MeshInstance3D = $MeshInstance3D
+		var mesh: CylinderMesh = mesh_node.get_mesh()
+		mesh.top_radius = radius
+		mesh.bottom_radius = radius
+		
+		mesh_node.position = Vector3(position.x + radius, position.y, position.z + radius)
