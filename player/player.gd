b2a7d28d3cb6b91985bd880f5dc798b0afc9c551
Pete Thorne <pete.thorne@learningpool.com>
2022-11-08 20:52:14 +0000

    Basic controls in place

--- player/player.gd
+++ player/player.gd
@@ -3,7 +3,11 @@ extends CharacterBody3D
 class_name Player
 
 
-@export var SPEED = 0.0
+@export var speed := 0.0
+@export var MAX_SPEED := 2.0
+@export var ROTATION_SPEED := 45.0
+@export var ACCELERATION := 1.0
+
 const JUMP_VELOCITY = 4.5
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
@@ -22,22 +26,22 @@ func _physics_process(delta: float) -> void:
 	var direction := (transform.basis * Vector3(0, 0, 1)).normalized()
 	
 	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+		velocity.x = direction.x * speed
+		velocity.z = direction.z * speed
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, speed)
+		velocity.z = move_toward(velocity.z, 0, speed)
 
 	move_and_slide()
 
 
-func forward():
-	SPEED = 1.5
+func forward(delta: float):
+	speed = clamp(speed + (delta * ACCELERATION), 0, MAX_SPEED)
 
 
-func rotate_left():
-	rotation.y += deg_to_rad(90)
+func rotate_left(delta: float):
+	rotation.y += deg_to_rad(delta * ROTATION_SPEED)
 
 
-func rotate_right():
-	rotation.y -= deg_to_rad(90)
+func rotate_right(delta: float):
+	rotation.y -= deg_to_rad(delta * ROTATION_SPEED)
