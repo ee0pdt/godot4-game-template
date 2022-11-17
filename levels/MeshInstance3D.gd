9ebcce37c49fd53594bc60786115d7cfc26afec8
Pete Thorne <pete.thorne@learningpool.com>
2022-11-17 11:41:31 +0000

    Better portal shader

---
+++ levels/MeshInstance3D.gd
@@ -0,0 +1,12 @@
+extends MeshInstance3D
+@tool
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	rotation.y += deg_to_rad(delta * 100)
