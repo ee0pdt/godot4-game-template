018c3ef307d1e5880492f29666e05965ba689ed5
Pete Thorne <pete.thorne@learningpool.com>
2022-11-17 11:55:36 +0000

    Messing with emission

--- levels/MeshInstance3D.gd
+++ levels/MeshInstance3D.gd
@@ -9,4 +9,5 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	rotation.y += deg_to_rad(delta * 100)
+	pass
+#	rotation.y += deg_to_rad(delta * 100)
